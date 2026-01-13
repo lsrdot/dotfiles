78ff5039d60e317ae9140a2d10062b566bd89feb
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2026-01-12 23:22:12 -0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
-
     set -g fish_greeting ""
     set -Ua fish_user_paths ~/.cargo/bin
 
@@ -7,31 +6,51 @@ if status is-interactive
 
     set -gx QT_QPA_PLATFORMTHEME qt5ct
 
-    set -gx LS_COLORS "di=38"
+    set -gx LS_COLORS "\
+di=01;38;2;255;255;255:\
+fi=38;2;180;180;180:\
+ln=38;2;220;220;220:\
+ex=01;38;2;240;240;240:\
+*.txt=38;2;160;160;160:\
+*.md=38;2;200;200;200:\
+*.json=38;2;190;190;190:\
+*.yaml=38;2;190;190;190:\
+*.yml=38;2;190;190;190:\
+pi=38;2;150;150;150:\
+so=38;2;170;170;170:\
+bd=38;2;140;140;140:\
+cd=38;2;140;140;140:\
+or=38;2;120;120;120:\
+mi=38;2;120;120;120:\
+*.tar=38;2;130;130;130:\
+*.zip=38;2;130;130;130:\
+*.gz=38;2;130;130;130"
+
+    set -g fish_color_normal white
+    set -g fish_color_command white --bold
+    set -g fish_color_param brwhite
+    set -g fish_color_error white --bold
+    set -g fish_color_comment 555555
+    set -g fish_color_quote brwhite
+    set -g fish_color_redirection white
+    set -g fish_color_end white
+    set -g fish_color_operator brwhite
 
     alias top="btop"
     alias vi="nvim"
     alias sysup="sudo pacman -Syu --noconfirm && yay -Syu --noconfirm && reboot"
 
-    set -gx PATH $PATH ~/.local/share/JetBrains/Toolbox/scripts
-
     set -x EDITOR "code --wait"
     set -x VISUAL "code --wait"
 
-    set -U fish_color_valid_path ''
-    set -U fish_color_match normal
-    set -U fish_color_completion normal
-    set -U fish_color_selection normal
-    set -U fish_pager_color_prefix normal
-    set -U fish_pager_color_completion normal
-    set -U fish_pager_color_description normal
-    set -U fish_color_autosuggestion 555
+    set -gx PATH $PATH ~/.local/share/JetBrains/Toolbox/scripts
+    set -gx PNPM_HOME "/home/dot/.local/share/pnpm"
 
+    fish_add_path /home/dot/.spicetify
+    fish_add_path ~/.spicetify
+
+    if not string match -q -- $PNPM_HOME $PATH
+        set -gx PATH "$PNPM_HOME" $PATH
+    end
 end
 
-fish_add_path /home/dot/.spicetify
-
-set -gx PNPM_HOME "/home/dot/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
--- hypr/shaders/scanline.frag
+++ hypr/shaders/scanline.frag
@@ -1,4 +1,3 @@
-// ~/.config/hypr/shaders/scanline.frag
 void main() {
     vec2 uv = gl_FragCoord.xy / resolution;
     float scanline = sin(uv.y * resolution.y * 3.1415 * 2.0) * 0.1 + 0.9;
