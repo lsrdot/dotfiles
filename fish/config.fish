c410274de10b6b624dfa5e155a31326b6ebed2cd
lsrdot <lucas.siqueiraribeiro99@gmail.com>
2026-01-25 12:20:57 -0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,20 @@
 if status --is-interactive
-    
-    
-    set -g fish_greeting ""
+    # Configurações iniciais
     set -Ua fish_user_paths ~/.cargo/bin
     starship init fish | source
 
-    if not set -q TMUX
+    # Enable vi mode
+    #fish_vi_key_bindings
+
+# Cursor shapes (Vim-like)
+    set -U fish_cursor_default block      # normal mode
+    set -U fish_cursor_insert block        # insert mode
+    set -U fish_cursor_visual block       # visual mode
+    set -U fish_cursor_replace_one block  # replace mode
+
+
+    # # Auto-start TMUX
+    if not set -q TMUX; and set -q HYPRLAND_INSTANCE_SIGNATURE
         set -l session_name "main" 
 
         if tmux has-session -t $session_name 2>/dev/null
@@ -16,14 +25,16 @@ if status --is-interactive
         end
     end
 
-set -gx LS_COLORS "rs=0:di=1;38;2;134;255;85:fi=38;2;111;230;63:ln=38;2;111;230;63:ex=38;2;124;255;74:pi=38;2;111;230;63:so=38;2;111;230;63:bd=38;2;111;230;63:cd=38;2;111;230;63:mi=38;2;111;230;63"
+    set -gx LS_COLORS "rs=0:di=1;38;2;51;255;102:fi=0;38;2;46;230;92:ln=38;2;51;255;102:ex=1;38;2;51;255;102:pi=38;2;51;255;102:so=38;2;51;255;102:bd=38;2;51;255;102:cd=38;2;51;255;102:mi=38;2;51;255;102"
 
+    # Notificação sonora em caso de erro
     function post_command_flash --on-event fish_postexec
         if test $status -ne 0
             printf \a
         end
     end
 
+    # Aliases
     alias vi="nvim"
     alias s="yay -Ss"
     alias i="yay -Sy"
@@ -36,8 +47,10 @@ set -gx LS_COLORS "rs=0:di=1;38;2;134;255;85:fi=38;2;111;230;63:ln=38;2;111;230;
     alias weather="curl wttr.in/Belo+Horizonte"
     alias ping="gping"
     alias fake="genact"
-    
+    alias scorp="/home/dot/.scripts/scorp.fish"
+    #alias ls="ls -lahs"
 
+    # Variáveis de ambiente
     set -x EDITOR "code --wait"
     set -x VISUAL "code --wait"
     set -gx PATH $PATH ~/.local/share/JetBrains/Toolbox/scripts
@@ -47,43 +60,51 @@ set -gx LS_COLORS "rs=0:di=1;38;2;134;255;85:fi=38;2;111;230;63:ln=38;2;111;230;
     fish_add_path /home/dot/.spicetify
     fish_add_path ~/.spicetify
 
+    
+    # function fish_greeting
+    #     set -l logo \
+    #     " " \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣶⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⢀⣠⣴⣶⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⡀⠀⠀⠀⠀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⡆⠀⢀⣴⣿⣿⣿⣿⠿⠟⠛⠛⠛⠻⠿⣿⣿⣿⣿⣦⡀⠀⢰⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⠟⠉⠀⣴⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣦⠀⠉⠻⢿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡿⠋⠀⠀⠀⣼⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣧⠀⠀⠀⠙⢿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⡿⠉⠀⠀⠀⠀⢰⣿⣿⣿⠁⠀⠀⠀⠀⣤⣾⣿⣿⣿⣷⣦⠀⠀⠀⠀⠈⣿⣿⣿⡆⠀⠀⠀⠀⠈⠻⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⡿⡏⠀⠀⠀⠀⠀⠀⣼⣿⣿⡏⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⢸⣿⣿⣷⠀⠀⠀⠀⠀⠀⢹⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⢿⣿⣿⡇⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣷⡄⠀⠀⠀⠀⠀⢸⣿⣿⣷⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⣼⣿⣿⡏⠀⠀⠀⠀⠀⢀⣾⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣦⣄⠀⠀⠀⠀⢿⣿⣿⣇⠀⠀⠀⠀⠀⠉⠛⠛⠛⠉⠀⠀⠀⠀⠀⣸⣿⣿⣿⠁⠀⠀⠀⣠⣴⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⡀⠀⠘⢿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⡿⠃⠀⢀⣠⣾⣿⣿⠿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⡀⠈⠻⢿⣿⣿⣶⠀⠀⠻⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⣠⣤⣾⣿⣿⡿⠏⠁⠀⣴⣿⣿⣿⠟⠉⢀⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⢿⠇⠀⠀⠀⠉⠛⠛⠀⠀⠀⠈⠙⠿⣿⣿⣿⣷⠀⠀⠀⣾⣿⣿⣿⠿⠁⠀⠀⠀⠀⠙⠛⠋⠀⠀⠀⠸⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⠀⠀⠀⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣵⣷⣄⠀⠀⠀⠀⠀⢀⣿⣿⣿⠀⠀⠀⣿⣿⣿⡄⠀⠀⠀⠀⠀⣠⣾⣷⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣷⣦⣄⣀⣤⣾⣿⡿⠋⠀⠀⠀⠘⣿⣿⣷⣤⣀⣠⣤⣾⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" \
+    #         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⠿⣿⣿⡟⠛⠁⠀⠀⠀⠀⠀⠀⠈⠘⠿⢿⣿⣿⠻⠏⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+
+        
+    #     for line in $logo
+    #         echo $line
+    #         sleep 0.03 
+    #     end
+    #     set_color normal
+    # end
+
     function fish_greeting
-    set -l logo \
-        " "\
-        "                  ▄" \
-        "                 ▄█▄" \
-        "                ▄███▄" \
-        "               ▄█████▄" \
-        "              ▄███████▄" \
-        "             ▄ ▀▀██████▄" \
-        "            ▄██▄▄ ▀█████▄" \
-        "           ▄█████████████▄" \
-        "          ▄███████████████▄" \
-        "         ▄█████████████████▄" \
-        "        ▄███████████████████▄" \
-        "       ▄█████████▀▀▀▀████████▄" \
-        "      ▄████████▀      ▀███████▄" \
-        "     ▄█████████        ████▀▀██▄" \
-        "    ▄██████████        █████▄▄▄" \
-        "   ▄██████████▀        ▀█████████▄" \
-        "  ▄██████▀▀▀              ▀▀██████▄" \
-        " ▄███▀▀                       ▀▀███▄" \
-        "▄▀▀                               ▀▀▄"
+    set -l ff (fastfetch --pipe false)
 
-    set_color 33ff33 
-
-    for line in $logo
+    for line in $ff
         echo $line
-        sleep 0.03 
+        sleep 0.02
+    end
     end
 
-    set_color normal
-    echo "" 
-    end
-
-
-
+    # Configuração do PNPM Path
     if not string match -q -- $PNPM_HOME $PATH
         set -gx PATH "$PNPM_HOME" $PATH
     end
-end
+end 
---
+++ fish/functions/scorp.fish
@@ -0,0 +1,3 @@
+function scorp --wraps=/home/dot/.scripts/scorp.fish --description 'alias scorp=/home/dot/.scripts/scorp.fish'
+    /home/dot/.scripts/scorp.fish $argv
+end
